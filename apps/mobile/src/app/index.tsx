8a230beab7e0cd0c7f85130c40a96849d629d139
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-09-03 09:24:53 -0300

    feat: place Kether behind Atrium gate

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,5 +1,10 @@
+import { AtriumGate } from '../features/auth/AtriumGate';
 import { Day001VerticalSlice } from '../features/kether/Day001VerticalSlice';
 
 export default function HomeScreen() {
-  return <Day001VerticalSlice />;
+  return (
+    <AtriumGate>
+      <Day001VerticalSlice />
+    </AtriumGate>
+  );
 }
